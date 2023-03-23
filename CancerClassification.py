1b3a8ecc05b38acb54ec6a64b88dd068ea68ea1b
jaisingh-25 <113584679+jaisingh-25@users.noreply.github.com>
2023-03-23 19:51:28 +0530

    Add files via upload

---
+++ CancerClassification.py
@@ -0,0 +1,34 @@
+# Importing the libraries
+import pandas as pd
+
+# Importing cancer dataset
+dataset = pd.read_csv(r'C:\Users\jaisi\Documents\Jai\Programs\College\Logistic Regression\data.csv')
+X = dataset.iloc[:, 2:31].values
+Y = dataset.iloc[:, 1].values
+
+# Encoding categorical data values
+from sklearn.preprocessing import LabelEncoder
+labelencoder_Y = LabelEncoder()
+Y = labelencoder_Y.fit_transform(Y)
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.25)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+# Using Logistic Regression Algorithm to the Training Set
+from sklearn.linear_model import LogisticRegression
+classifier = LogisticRegression(random_state = 0)
+classifier.fit(X_train, Y_train)
+Y_pred = classifier.predict(X_test)
+
+# Calculating accuracy from confusion matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(Y_test, Y_pred)
+accuracy = (cm[0,0]+cm[1,1])/(cm[0,0]+cm[1,1]+cm[0,1]+cm[1,0])
+print(accuracy*100,'%')
---
+++ HousingPrice.py
@@ -0,0 +1,28 @@
+# Created by Jai_RA2212701010011
+
+# Importing modules
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Reading dataset and assigning x and y
+raw_data = pd.read_csv(r'C:\Users\jaisi\Documents\Jai\Programs\College\Linear Regression\ParisHousing.csv')
+x = raw_data[['squareMeters', 'numberOfRooms', 'hasYard', 'hasPool', 'floors', 'cityCode', 'cityPartRange',
+              'numPrevOwners', 'made', 'isNewBuilt', 'hasStormProtector', 'basement', 'attic', 'garage',
+              'hasStorageRoom', 'hasGuestRoom']]
+y = raw_data['price']
+
+# Splitting into test and train data
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 100)
+
+# Applying linear regression
+from sklearn.linear_model import LinearRegression
+model = LinearRegression()
+model.fit(x_train, y_train)
+predictions = model.predict(x_test)
+plt.scatter(y_test, predictions)
+
+# Calculating mean squared error
+from sklearn import metrics
+mse=metrics.mean_squared_error(y_test, predictions)
+print(mse)
