4efcbe55e2e4a68a6c0c681386ef5cb31739c572
jaisingh-25 <113584679+jaisingh-25@users.noreply.github.com>
2024-04-01 22:12:13 +0530

    Update CancerClassification.py

--- CancerClassification.py
+++ CancerClassification.py
@@ -8,7 +8,6 @@ Y = dataset.iloc[:, 1].values
 from sklearn.preprocessing import LabelEncoder
 labelencoder_Y = LabelEncoder()
 Y = labelencoder_Y.fit_transform(Y)
-
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.25)
