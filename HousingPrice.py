6f1891060dd96f181bccb8a9298c424c748a1787
jaisingh-25 <113584679+jaisingh-25@users.noreply.github.com>
2024-04-28 00:21:56 +0530

    Update HousingPrice.py

--- HousingPrice.py
+++ HousingPrice.py
@@ -7,8 +7,6 @@ x = raw_data[['squareMeters', 'numberOfRooms', 'hasYard', 'hasPool', 'floors', '
 y = raw_data['price']
 from sklearn.model_selection import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 100)
-
-# Applying linear regression
 from sklearn.linear_model import LinearRegression
 model = LinearRegression()
 model.fit(x_train, y_train)
